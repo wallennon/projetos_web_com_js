5705217bfeb0434c85772fa46db0c90f1538357a
wallennon <wallennon@gmail.com>
2023-10-05 18:58:57 -0300

    First commit

---
+++ assets/js/acordeon.js
@@ -0,0 +1,14 @@
+const acordeonTriggres = document.querySelectorAll('.acordeon .trigger')
+
+acordeonTriggres.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+        const acordeon = trigger.parentElement
+        const isOpen = acordeon.classList.contains('open')
+
+        if (isOpen) {
+            acordeon.classList.remove('open')
+        } else {
+            acordeon.classList.add('open')
+        }
+    })
+})
